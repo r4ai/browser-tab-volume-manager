8eb267c4865badf3dc4a6d25dbf2c9b1193c44d4
rai <mj82xx@gmail.com>
2023-06-21 23:15:49 +0900

    Fix lint command to run all lint scripts

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,7 +1,6 @@
 import '../global.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 import Options from './Options';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
