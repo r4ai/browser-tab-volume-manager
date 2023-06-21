385f8b6ef8bacfef2fffc721469c4361167a6468
rai <mj82xx@gmail.com>
2023-06-21 22:05:03 +0900

    Delete Popup.spec.tsx test file

--- src/popup/Popup.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Popup from './Popup';
-
-it('should load and display Popup', async () => {
-  render(<Popup />);
-
-  expect(screen.getByText('Volume Control')).toBeInTheDocument();
-});
